6a0cdf7ed7364fe9879a4b54faf263a8ea7447ad
xinlingqudongX <chenpeng10810@gmail.com>
2022-12-10 18:43:10 +0800

    修改sercret_key

--- DjangoTemplate/DjangoTemplate/settings.py
+++ DjangoTemplate/DjangoTemplate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u3hg9(h!1k!fgj$3la=v=0segf1qs4!-qoegk=^nt&7a$r%1ou"
+# SECRET_KEY = "django-insecure-u3hg9(h!1k!fgj$3la=v=0segf1qs4!-qoegk=^nt&7a$r%1ou"
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
