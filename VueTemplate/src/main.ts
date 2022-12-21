1ec77bb90ab3d69e20c38596c621df50b0ef169f
xinlingqudongX <chenpeng10810@gmail.com>
2022-12-21 21:53:13 +0800

    修改错误

--- VueTemplate/src/main.ts
+++ VueTemplate/src/main.ts
@@ -3,8 +3,11 @@ import App from './App.vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import Router from './router';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+
+app.use(createPinia());
 app.use(ElementPlus);
 app.use(Router);
 app.mount('#app');
