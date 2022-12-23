951039c18e8ea492c12a13b32b2cf07e1d43c0a3
xinlingqudongX <chenpeng10810@gmail.com>
2022-12-24 00:34:40 +0800

    添加eslint包和icon的导入

--- VueTemplate/src/main.ts
+++ VueTemplate/src/main.ts
@@ -4,8 +4,12 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import Router from './router';
 import { createPinia } from 'pinia';
+import * as Icons from '@element-plus/icons-vue';
 
 const app = createApp(App);
+for (const [key, componet] of Object.entries(Icons)) {
+    app.component(key, componet);
+}
 
 app.use(createPinia());
 app.use(ElementPlus);
